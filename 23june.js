1c93b2d9e776a6cb3e9370eeb6500a3dc6a4be0a
YaminiSinha05 <142322288+YaminiSinha05@users.noreply.github.com>
2023-08-15 19:19:43 +0530

    Add files via upload

---
+++ 23june.js
@@ -0,0 +1,22 @@
+<html>
+   <head>  
+      <script type = "text/javascript">
+         function concatenate(first, last) {
+            var full;
+            full = first + last;
+            return full;
+         }
+         function secondFunction() {
+            var result;
+            result = concatenate('Ram', 'Prasad');
+            document.write (result );
+         }
+      </script>      
+   </head>
+   
+   <body>
+      <script>
+          secondFunction()
+      </script> 
+  </body>
+</html>
